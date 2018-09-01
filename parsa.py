328901ec7a1a979f1bf1c76820edba8be6b133a4
post-rick <riccardodimaio11@gmail.com>
2018-09-01 19:16:50 +0200

    Add argument parsing

--- parsa.py
+++ parsa.py
@@ -2,17 +2,24 @@ import argparse
 import os
 import textract
 
-# Set directory to current working directory
-directory = os.getcwd()
-
 parser = argparse.ArgumentParser(description='Text parser that supports most text file extensions. Writes the output for each file to [filename].txt.')
 
-parser.add_argument('input', help='The input can either be a file or a folder; in the latter case, all the supported files located inside the folder will be scanned.')
-# TODO - stats
-parser.add_argument('--stats', '-s', nargs='?', help='Output stats file.')
-parser.add_argument('--outfolder', '-o', nargs='?', default=None, help='Output folder where the output files will be stored. By default, it will be a folder named parsaoutput located in the input folder.')
+# TODO - improve argument descriptions
+parser.add_argument('input', help='input file or folder; if a folder is passed as input, parsa will scan every file inside it recursively (scanning subfolders as well)')
+parser.add_argument('--stats', '-s', nargs='?', help='output stats file')
+parser.add_argument('--output', '-o', nargs='?', default=None, help='output folder where the output files will be stored. If the input is a file, the default output directory is the input file\'s directory; otherwise, it\'s a folder named \'parsaoutput\' in the input folder.')
 
+# Parse arguments into NameSpace objects
 args = parser.parse_args()
+
+# Check if input is file or dir
+if os.path.isfile(args.input):
+    # If output directory wasn't provided, set it to the input directory
+    if args.output == None:
+        args.output = os.path.dirname(args.input)
+    # Make output folder
+    args.output = os.path.join(args.output, 'parsaoutput')
+    print(args.output)
 
 # if outfolder = None set outfolder = infolder
 
