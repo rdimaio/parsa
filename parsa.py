0797d10d40f7b5446279314606b7e4995991af6e
post-rick <riccardodimaio11@gmail.com>
2018-09-04 20:55:00 +0200

    Add utf-8 decoding instead of string typecasting

--- parsa.py
+++ parsa.py
@@ -6,6 +6,7 @@ import textract
 # TODO - document functions
 
 def set_outdir(args_outdir, indir):
+    """Set output directory based on whether a custom outside directory was provided or not, and return it."""
     # If output directory wasn't provided, set it to the input directory
     if args_outdir == None:
         outdir = indir
@@ -14,33 +15,36 @@ def set_outdir(args_outdir, indir):
     return outdir
 
 def parse_arguments():
-    argparser = argparse.ArgumentParser(description='Textract-based text parser that supports most text file extensions. Writes the output for each file to [filename].txt.')
+    """Parse command-line arguments, and return a Namespace object containing them."""
     # TODO - improve argument descriptions, break strings into shorter codelines
+    argparser = argparse.ArgumentParser(description='Textract-based text parser that supports most text file extensions. Writes the output for each file to [filename].txt.')
     argparser.add_argument('input', help='input file or folder; if a folder is passed as input, parsa will scan every file inside it recursively (scanning subfolders as well)')
     argparser.add_argument('--stats', '-s', nargs='?', help='output stats file')
     argparser.add_argument('--output', '-o', nargs='?', default=None, help='output folder where the output files will be stored. If the input is a file, the default output directory is the input file\'s directory; otherwise, it\'s a folder named \'parsaoutput\' in the input folder.')
-
-    # Parse arguments into NameSpace objects
+    # Parse arguments into Namespace
     args = argparser.parse_args()
+    return args
 
-    return(args)
-
+# Get CLI arguments
 args = parse_arguments()
 
 # If input is a file
 if os.path.isfile(args.input):
 
+    # Set IO variables
     infile = args.input
     outdir = set_outdir(args.output, os.path.dirname(infile))
 
+    # Get input's filename with neither its path nor extension
     filename_noextension = os.path.basename(os.path.normpath(os.path.splitext(infile)[0]))
 
-    # os.path.join creates paths that work in multiple OSes
+    # Create path for output file
+    # os.path.join intelligently creates filepaths that work cross-platform
     outfile = os.path.join(outdir, filename_noextension) + '.txt'
 
     # Extract text
-    # TODO - maybe need to decode instead of typecast to str
-    text = str(textract.process(infile))
+    # utf-8 is used here as it's able to handle all languages efficiently (https://stackoverflow.com/a/2438901)
+    text = textract.process(infile).decode("utf-8")
 
     # TODO - maybe make a recursive function out of this to improve it;
     # TODO - maybe just needs a while loop
