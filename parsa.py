97adfe30b3618cb4b6c0bbfa01ccf0eb756a2155
post-rick <riccardodimaio11@gmail.com>
2018-09-16 19:57:03 +0200

    Restructure project to improve readability in main file

--- parsa.py
+++ parsa.py
@@ -2,9 +2,12 @@ import argparse
 import os
 import textract
 import time
-from cli import *
-from filesystem import *
-from text import *
+#import filesystem
+# from filesystem import *
+#from filesystem import *
+import utils.cli as cli
+import utils.filesystem as fs
+import utils.text as txt
 
 # timing tool
 # t0 = time.time()
@@ -12,7 +15,7 @@ from text import *
 #     print(t1-t0)
 
 # Get CLI arguments
-args = parse_arguments()
+args = cli.parse_arguments()
 
 # If input is a file
 if os.path.isfile(args.input):
@@ -20,16 +23,16 @@ if os.path.isfile(args.input):
     # Set IO variables
     infile = args.input
     indir = os.path.dirname(infile)
-    outdir = set_outdir(args.output, indir)
+    outdir = fs.set_outdir(args.output, indir)
 
     # Extract text and write it to the output file
-    text = get_text(infile)
+    text = txt.get_text(infile)
     
     # Write to outfile if text has been extracted successfully
     if text:
-        outfile = compose_unique_filepath(infile, outdir)
+        outfile = fs.compose_unique_filepath(infile, outdir)
         try:
-            write_str_to_file(text, outfile) 
+            fs.write_str_to_file(text, outfile) 
         except OSError as e:
             print(e)
     
@@ -38,19 +41,19 @@ elif os.path.isdir(args.input):
 
     # Set IO variables
     indir = args.input
-    outdir = os.path.join(set_outdir(args.output, indir), 'parsaoutput')
+    outdir = os.path.join(fs.set_outdir(args.output, indir), 'parsaoutput')
 
     # Create output folder
     os.makedirs(outdir, exist_ok=True)
 
-    filelist = get_filelist(indir)
+    filelist = fs.get_filelist(indir)
 
     for infile in filelist:
-        text = get_text(infile)
+        text = txt.get_text(infile)
         if text:
-            outfile = compose_unique_filepath(infile, outdir)
+            outfile = fs.compose_unique_filepath(infile, outdir)
             try:
-                write_str_to_file(text, outfile) 
+                fs.write_str_to_file(text, outfile) 
             except OSError as e:
                 print(e)
 
