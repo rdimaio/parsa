862eb368c0eb0674be05e80b174a2509aea7ce7c
post-rick <riccardodimaio11@gmail.com>
2018-09-22 03:52:53 +0200

    Update structure

---
+++ parsa/utils/cli.py
@@ -0,0 +1,44 @@
+"""utils/cli.py - Command-line utilities for parsa
+
+Classes:
+    _SmartFormatter - allows formatting in the CLI help menu
+
+Functions:
+    parse_arguments - parse CLI arguments
+    _set_arguments - set CLI description and arguments
+"""
+
+import argparse
+
+class _SmartFormatter(argparse.HelpFormatter):
+    """Allows formatting in the CLI help menu.
+    Called by beginning a string with R| in _set_arguments().
+    https://stackoverflow.com/a/22157136
+    """
+    def _split_lines(self, text, width):
+        if text.startswith('R|'):
+            return text[2:].splitlines()  
+        # this is the RawTextHelpFormatter._split_lines
+        return argparse.HelpFormatter._split_lines(self, text, width)
+
+def parse_arguments():
+    """Parse command-line arguments, and return a Namespace object containing them."""
+    argparser = _set_arguments()
+    # Parse arguments into Namespace
+    args = argparser.parse_args()
+    return args
+
+def _set_arguments():
+    """Set CLI description and arguments."""
+    argparser = argparse.ArgumentParser(description=('Textract-based text parser that supports most text file extensions. '
+    'Parsa can parse multiple formats at once, ' 
+    'writing them to .txt files in the directory of choice.'), formatter_class=_SmartFormatter)
+
+    argparser.add_argument('input', help=('input file or folder; if a folder is passed as input, '
+    'parsa will scan every file inside it recursively (scanning subfolders as well)'))
+
+    argparser.add_argument('--output', '-o', nargs='?', default=None, help=('R|folder where the output files '
+    'will be stored. The default folder is: \n'
+    '(a) the input file\'s parent folder, if the input is a file, or \n'
+    '(b) a folder named \'parsaoutput\' located in the input folder, if the input is a folder.'))
+    return argparser
---
+++ parsa/utils/text.py
@@ -0,0 +1,60 @@
+"""utils/text.py - Text utilities for parsa
+
+Functions:
+    get_text - extract text from the input file
+    _process_text - process extracted text and return it as a simple string
+"""
+
+import os
+import textract
+
+def get_text(infile, _infile_extension=None):
+    """Extract text from the input file using textract, returning an empty string if failing to do so.
+    If the infile does not explicitly have an extension (UnicodeDecodeError), 
+    the user will be prompted to input the correct extension (either with or without a dot). 
+    get_text is then recursively called with _infile_extension set to the input extension.
+
+    The caller should never set _infile_extension to anything in most cases, 
+    unless they want to skip the prompt for input extension and the entirety of the input is of the same format.
+    """
+    # If text is not extracted or the infile is empty, the function will just return an empty string
+    text = ''   
+
+    try:
+        text = textract.process(infile, extension=_infile_extension)
+    # File existence gets checked in parsa.py
+    except textract.exceptions.ExtensionNotSupported:
+        print("Error while parsing file: " + infile)
+        print("Extension not supported\n")
+    # Skip file if parsing has failed
+    except textract.exceptions.ShellError as e:
+        print("Error while parsing file: " + infile)
+        print(e)
+    # If the file has no explicit extension, prompt the user for it
+    except UnicodeDecodeError:
+        print("Error while parsing file: " + infile)
+        print("File has no extension\n")
+
+        # Prompt the user for the input file's extension
+        # textract.process adds a dot before the input extension if it's not already present (e.g. txt -> .txt)
+        _infile_extension = input("Please input the file's extension (e.g. .pdf or pdf):")
+        
+        # Call the function again; an exception will be raised on failure
+        text = get_text(infile, _infile_extension)
+    # If no exceptions happened, format text adeguately
+    else:
+        # Extract input file's extension unless it has already been specified
+        if not _infile_extension:
+            _infile_extension = os.path.splitext(infile)[1]
+
+        text = _process_text(text, _infile_extension)
+    return text
+
+def _process_text(text, _infile_extension):
+    """Process extracted text and return it as a simple string."""
+    # utf-8 is used here to handle different languages efficiently (https://stackoverflow.com/a/2438901)
+    text = text.decode('utf-8')
+    # Remove unnecessary trailing space caused by the form feed (\x0c, \f) character at the end of .pdf files
+    if _infile_extension == '.pdf' or _infile_extension == 'pdf':
+        text = text.strip()
+    return text
