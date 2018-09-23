4d2a3f4124c8040316e4141285e2a8ba19dbca15
post-rick <riccardodimaio11@gmail.com>
2018-09-23 17:22:30 +0200

    Complete set_oudir tests

--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -1,7 +1,6 @@
 import unittest, os, sys
+
 sys.path.append(os.path.abspath('..'))
-#from parsa.utils.filesystem import *
-#from parsa.utils import filesystem as fs
 from parsa.utils import filesystem as fs
 
 
@@ -12,13 +11,14 @@ class FileSystemTestCase(unittest.TestCase):
     def test_get_filelist(self):
         return True
 
-    def test_set_outdir(self):
-        outdir = fs.set_outdir('foo', 'bar')
-        self.assertEqual(outdir, 'foo')
+    def test_set_outdir_with_outdir_provided(self):
+        outdir = fs.set_outdir('args_outdir', 'indir')
+        self.assertEqual(outdir, 'args_outdir')
 
     def test_set_outdir_no_outdir_provided(self):
-        outdir = fs.set_outdir(None, 'bar')
-        self.assertEqual(outdir, 'bar')
+        '''If no args_outdir is provided, then the outdir will be set to the indir.'''
+        outdir = fs.set_outdir(None, 'indir')
+        self.assertEqual(outdir, 'indir')
     
     def test_write_str_to_file(self):
         return True
