4fcfc1936740019fd0a6512a39336a7df944e6f6
post-rick <riccardodimaio11@gmail.com>
2018-09-25 00:58:48 +0200

    Add test_text.py

--- tests/test_text.py
+++ tests/test_text.py
@@ -1,8 +1,10 @@
 import unittest
 
 class TextTestCase(unittest.TestCase):
-    def test_get_text:
+    def test_get_text(self):
         """Test that """
+        return True
     
-    def test__process_text:
+    def test__process_text(self):
         """Test that text is successfully processed."""
+        return True
