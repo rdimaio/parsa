d36be6f08d14ff985b2bc7eed510a83dd00b2392
post-rick <riccardodimaio11@gmail.com>
2018-09-25 00:58:11 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Parsa',
+      version='1.0',
+      description='Multiformat text parser',
+      author='Riccardo Di Maio',
+      author_email='rdimaio@protonmail.com',
+      url='https://github.com/rdimaio/parsa',
+      packages=['distutils', 'distutils.command'],
+     )
