84f58d9350ae9794b11e2bace9faaf951b1122f6
post-rick <riccardodimaio11@gmail.com>
2018-09-26 01:19:20 +0200

    Add tests for compose_unique_filepath

--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -8,17 +8,37 @@ from parsa.utils import filesystem as fs
 
 
 class FileSystemTest(unittest.TestCase):
-    def test_compose_unique_filepath_with_unique_input(self):
-        """Compose a filepath for test.pdf, with no file named 'test.txt' in the output directory"""
+    def test_compose_unique_filepath_with_no_outfile_in_outdir(self):
+        """Compose an output filepath for 'foo.pdf', with no file named 'foo.txt' in the output directory.
+        Expected output: 'foo.txt'
+        """
+        infile = 'foo.pdf'
+        # Get directory where temporary files are created
+        outdir = tempfile.gettempdir()
+        outfile = fs.compose_unique_filepath(infile, outdir)
+        expected_outfile = os.path.join(outdir, 'foo.txt')
+        self.assertEqual(outfile, expected_outfile)
 
-        #outfile = fs.compose_unique_filepath()
-        #self.assertEqual(outfile, 'outfile.')
-        return True
+    # TODO - maybe create a class and setUp just for these where I'm making a folder with temp files inside
+    # teardown too, so that the files don't persist between tests
+    def test_compose_unique_filepath_with_outfile_existing_in_outdir(self):
+        """Compose an output filepath for 'foo.pdf', with a file named 'foo.txt' in the output directory.
+        Expected output: 'foo.pdf.txt'
+        """
+        infile = 'foo.pdf'
+        # Work in a temporary directory
+        with tempfile.TemporaryDirectory() as outdir:
+            # Create a temporary file named 'foo.txt'
+            existing_outfile = os.path.join(outdir, 'foo.txt')
+            with open(existing_outfile, 'w'):
+                outfile = fs.compose_unique_filepath(infile, outdir)
+            # Remove the temporary file
+            os.remove(existing_outfile)
+            expected_outfile = os.path.join(outdir, 'foo.pdf.txt')
+        self.assertEqual(outfile, expected_outfile)
 
     def test_compose_unique_filepath(self):
         # create temp files for different cases.
-        # first case, file is not already present -> outfile.txt
-        # second case, file is present -> outfile.pdf.txt
         # third case, file is present, outfile.pdf.txt is present -> outfile.pdf2.txt
         return True
 
@@ -32,7 +52,7 @@ class FileSystemTest(unittest.TestCase):
         self.assertEqual(outdir, args_outdir)
 
     def test_set_outdir_no_outdir_provided(self):
-        '''If no args_outdir is provided, then the outdir will be set to the indir.'''
+        '''If no args_outdir is provided, then outdir will be set to the indir.'''
         args_outdir = None
         indir = 'bar'
         outdir = fs.set_outdir(args_outdir, indir)
