a186dfb018dc4dd8e8d564fd3df2d527fab053f5
post-rick <riccardodimaio11@gmail.com>
2018-09-26 16:54:50 +0200

    Add different test for Python 2.x

--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -3,9 +3,6 @@ import os
 import sys
 import tempfile
 
-if sys.version_info[0] < 3:
-    from backports import tempfile
-
 sys.path.append(os.path.abspath('..'))
 from parsa.utils import filesystem as fs
 
@@ -30,14 +27,28 @@ class FileSystemTest(unittest.TestCase):
         """
         infile = 'foo.pdf'
         # Work in a temporary directory
-        with tempfile.TemporaryDirectory() as outdir:
-            # Create a temporary file named 'foo.txt'
-            existing_outfile = os.path.join(outdir, 'foo.txt')
-            with open(existing_outfile, 'w'):
-                outfile = fs.compose_unique_filepath(infile, outdir)
-            # Remove the temporary file
-            os.remove(existing_outfile)
-            expected_outfile = os.path.join(outdir, 'foo.pdf.txt')
+        # Python 2.x (mkdtemp() is used instead of TemporaryDirectory())
+        if sys.version_info[0] < 3:
+            with tempfile.mkdtemp() as outdir:
+                # Create a temporary file named 'foo.txt'
+                existing_outfile = os.path.join(outdir, 'foo.txt')
+                with open(existing_outfile, 'w'):
+                    outfile = fs.compose_unique_filepath(infile, outdir)
+                # Remove the temporary file
+                os.remove(existing_outfile)
+                # Remove the temporary directory (mdktemp must be manually deleted)
+                expected_outfile = os.path.join(outdir, 'foo.pdf.txt')    
+            os.remove(outdir)
+        # Python 3.x
+        else:
+            with tempfile.TemporaryDirectory() as outdir:
+                # Create a temporary file named 'foo.txt'
+                existing_outfile = os.path.join(outdir, 'foo.txt')
+                with open(existing_outfile, 'w'):
+                    outfile = fs.compose_unique_filepath(infile, outdir)
+                # Remove the temporary file
+                os.remove(existing_outfile)
+                expected_outfile = os.path.join(outdir, 'foo.pdf.txt')
         self.assertEqual(outfile, expected_outfile)
 
     def test_compose_unique_filepath(self):
