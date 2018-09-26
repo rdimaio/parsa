74cbdf651365189e1234f89f6abfb0646498c64a
post-rick <riccardodimaio11@gmail.com>
2018-09-26 18:38:07 +0200

    Added all cases for compose_unique_filepath in FilepathCompositionTest

--- tests/test_filesystem.py
+++ tests/test_filesystem.py
@@ -6,56 +6,186 @@ import tempfile
 sys.path.append(os.path.abspath('..'))
 from parsa.utils import filesystem as fs
 
+class FilepathCompositionTestCase(unittest.TestCase):
+    """Base class for tests regarding compose_unique_filepath."""
 
-class FileSystemTest(unittest.TestCase):
-    def test_compose_unique_filepath_with_no_outfile_in_outdir(self):
+    # Test infile name
+    infile = 'foo.pdf'
+
+    def generate_conflicts(self, conflicts_count, outdir):
+        """Generate conflicting filepaths for compose_unique_filepath.
+        conflicts_count specifies the number of conflicting filepaths to create.
+        First conflict: foo.txt
+        Second conflict: foo.pdf.txt
+        Third conflict onwards: foo.pdf2.txt, foo.pdf3.txt, etc.
+        """
+        if conflicts_count < 1:
+            raise ValueError('Number of conflicts to generate must be a positive non-zero integer.')
+        elif not isinstance(conflicts_count, int):
+            raise TypeError('Input must be an integer.')
+
+        conflicts = []
+
+        conflicts.append(os.path.join(outdir, 'foo.txt'))
+
+        if conflicts_count > 1:
+            conflicts.append(os.path.join(outdir, 'foo.pdf.txt'))
+
+        if conflicts_count > 2:
+            base_conflict_no_extension = os.path.join(outdir, 'foo')
+            for conflicts_counter in range(2, conflicts_count):
+                conflicts.append(base_conflict_no_extension + '.pdf' + str(conflicts_counter) + '.txt')
+        return(conflicts)
+
+
+# ===== TESTS =====
+class FilepathCompositionTest(FilepathCompositionTestCase):
+    """Tests involving compose_unique_filepath.
+    No setUp or tearDown functions are present,
+    as for most of these tests a different setUp and tearDown
+    is required for Python 2.x and Python 3.x.
+    Moreover, the tests involve operating
+    inside with statements, which need to be 
+    inside the test functions themselves.
+    """
+
+    def test_compose_unique_filepath_no_conflicts_in_outdir(self):
         """Compose an output filepath for 'foo.pdf', with no file named 'foo.txt' in the output directory.
         Expected output: 'foo.txt'
         """
-        infile = 'foo.pdf'
         # Get directory where temporary files are created
         outdir = tempfile.gettempdir()
-        outfile = fs.compose_unique_filepath(infile, outdir)
+        outfile = fs.compose_unique_filepath(self.infile, outdir)
         expected_outfile = os.path.join(outdir, 'foo.txt')
         self.assertEqual(outfile, expected_outfile)
 
-    # TODO - maybe create a class and setUp just for these where I'm making a folder with temp files inside
-    # teardown too, so that the files don't persist between tests
-    def test_compose_unique_filepath_with_existing_outfile_in_outdir(self):
-        """Compose an output filepath for 'foo.pdf', with a file named 'foo.txt' in the output directory.
+    def test_compose_unique_filepath_one_conflict_in_outdir(self):
+        """Compose an output filepath for 'foo.pdf', 
+        when a file named 'foo.txt' is already present in the output directory.
         Expected output: 'foo.pdf.txt'
         """
-        infile = 'foo.pdf'
-        # Work in a temporary directory
-        # Python 2.x (mkdtemp() is used instead of TemporaryDirectory())
+        # Python 2.x
+        # Differences with the Python 3.x test:
+        #   tempfile.mkdtemp() is used instead of tempfile.TemporaryDirectory()
+        #   shutil.rmtree is used instead of os.remove to remove the temporary directory
         if sys.version_info[0] < 3:
             import shutil
+            # Work in a temporary directory
             outdir = tempfile.mkdtemp()
             # Create a temporary file named 'foo.txt'
-            existing_outfile = os.path.join(outdir, 'foo.txt')
-            with open(existing_outfile, 'w'):
-                outfile = fs.compose_unique_filepath(infile, outdir)
+            conflicts = self.generate_conflicts(1, outdir)
+            with open(conflicts[0], 'w'):
+                outfile = fs.compose_unique_filepath(self.infile, outdir)
             # Remove the temporary file
-            os.remove(existing_outfile)
+            os.remove(conflicts[0])
             # Remove the temporary directory (mdktemp must be manually deleted)
-            # Source: https://stackoverflow.com/a/6129600
+            # shutil.rmtree is used instead of os.remove to avoid OSError
             shutil.rmtree(outdir, ignore_errors=True)
         # Python 3.x
         else:
+            # Work in a temporary directory
             with tempfile.TemporaryDirectory() as outdir:
                 # Create a temporary file named 'foo.txt'
-                existing_outfile = os.path.join(outdir, 'foo.txt')
-                with open(existing_outfile, 'w'):
-                    outfile = fs.compose_unique_filepath(infile, outdir)
+                conflicts = self.generate_conflicts(1, outdir)
+                with open(conflicts[0], 'w'):
+                    outfile = fs.compose_unique_filepath(self.infile, outdir)
                 # Remove the temporary file
-                os.remove(existing_outfile)
+                os.remove(conflicts[0])
         expected_outfile = os.path.join(outdir, 'foo.pdf.txt')
         self.assertEqual(outfile, expected_outfile)
 
-    def test_compose_unique_filepath(self):
-        # create temp files for different cases.
-        # third case, file is present, outfile.pdf.txt is present -> outfile.pdf2.txt
-        return True
+    def test_compose_unique_filepath_two_conflicts_in_outdir(self):
+        """Compose an output filepath for 'foo.pdf', 
+        when two files, named 'foo.txt' and 'foo.pdf.txt' respectively, are already present in the output directory.
+        Expected output: 'foo.pdf2.txt'
+        """
+        # Python 2.x
+        if sys.version_info[0] < 3:
+            import shutil
+            # Work in a temporary directory
+            outdir = tempfile.mkdtemp()
+            # Create two temporary files named 'foo.txt' and 'foo.pdf.txt'
+            conflicts = self.generate_conflicts(2, outdir)
+            with open(conflicts[0], 'w'), open (conflicts[1], 'w'):
+                outfile = fs.compose_unique_filepath(self.infile, outdir)
+            # Remove the temporary files
+            for conflict in conflicts:
+                os.remove(conflict)
+            # Remove the temporary directory (mdktemp must be manually deleted)
+            # shutil.rmtree is used instead of os.remove to avoid OSError
+            shutil.rmtree(outdir, ignore_errors=True)
+        # Python 3.x
+        else:
+            # Work in a temporary directory
+            with tempfile.TemporaryDirectory() as outdir:
+                # Create a temporary file named 'foo.txt'
+                conflicts = self.generate_conflicts(2, outdir)
+                with open(conflicts[0], 'w'), open (conflicts[1], 'w'):
+                    outfile = fs.compose_unique_filepath(self.infile, outdir)
+                # Remove the temporary files
+                for conflict in conflicts:
+                    os.remove(conflict)
+        expected_outfile = os.path.join(outdir, 'foo.pdf2.txt')
+        self.assertEqual(outfile, expected_outfile)
+
+    def test_compose_unique_filepath_ten_conflicts_in_outdir(self):
+        """Compose an output filepath for 'foo.pdf', 
+        when ten files, named 'foo.txt', 'foo.pdf.txt'
+        and 'foo.pdf2.txt' through 'foo.pdf9.txt' are present.
+        Expected output: 'foo.pdf10.txt'
+        """
+        # Python 2.x
+        if sys.version_info[0] < 3:
+            import shutil
+            # Work in a temporary directory
+            outdir = tempfile.mkdtemp()
+            # Create the temp files
+            conflicts = self.generate_conflicts(10, outdir)
+            # AFAIK, there is no completely safe way of opening a list of files using a with statement in Python 2.x
+            with open(conflicts[0], 'w'), \
+                 open (conflicts[1], 'w'), \
+                 open (conflicts[2], 'w'), \
+                 open (conflicts[3], 'w'), \
+                 open (conflicts[4], 'w'), \
+                 open (conflicts[5], 'w'), \
+                 open (conflicts[6], 'w'), \
+                 open (conflicts[7], 'w'), \
+                 open (conflicts[8], 'w'), \
+                 open (conflicts[9], 'w'):
+                outfile = fs.compose_unique_filepath(self.infile, outdir)
+            # Remove the temporary files
+            for conflict in conflicts:
+                os.remove(conflict)
+            # Remove the temporary directory (mdktemp must be manually deleted)
+            # shutil.rmtree is used instead of os.remove to avoid OSError
+            shutil.rmtree(outdir, ignore_errors=True)
+        # Python 3.x
+        else:
+            # Work in a temporary directory
+            with tempfile.TemporaryDirectory() as outdir:
+                # Create the temp files
+                conflicts = self.generate_conflicts(10, outdir)
+                # AFAIK, there is no completely safe way of opening a list of files using a with statement in Python 2.x
+                with open(conflicts[0], 'w'), \
+                     open (conflicts[1], 'w'), \
+                     open (conflicts[2], 'w'), \
+                     open (conflicts[3], 'w'), \
+                     open (conflicts[4], 'w'), \
+                     open (conflicts[5], 'w'), \
+                     open (conflicts[6], 'w'), \
+                     open (conflicts[7], 'w'), \
+                     open (conflicts[8], 'w'), \
+                     open (conflicts[9], 'w'):
+                    outfile = fs.compose_unique_filepath(self.infile, outdir)
+                # Remove the temporary files
+                for conflict in conflicts:
+                    os.remove(conflict)
+        expected_outfile = os.path.join(outdir, 'foo.pdf10.txt')
+        self.assertEqual(outfile, expected_outfile)
+    
+
+
+class FileSystemTest(unittest.TestCase):
 
     def test_get_filelist(self):
         return True
