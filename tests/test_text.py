c02370887ea4973d0c50791de5fb86d0b9d4b5f9
post-rick <riccardodimaio11@gmail.com>
2018-10-01 00:04:40 +0200

    Complete test suite for utils/text.py

--- tests/test_text.py
+++ tests/test_text.py
@@ -1,8 +1,13 @@
 """Tests for utils/text.py.
 Tests:
     get_text:
-        base cases for each format are not tested here, as they are already tested in textract's library
+        base tests for each format are not tested here, as they are already tested in textract's library
         (https://github.com/deanmalmgren/textract/tree/master/tests)
+
+        empty_file
+        extension_not_supported
+        no_extension
+
 
 
     
@@ -11,18 +16,65 @@ Tests:
         pdf
 """
 
+import textract
 import unittest
+from unittest import mock
 import os
 import sys
 import tempfile
 
+# Used to check print() output from utils/text.py
+# https://stackoverflow.com/a/34738440
+if sys.version_info[0] < 3:
+    import StringIO as io
+else:
+    import io
+
 sys.path.append(os.path.abspath('..'))
 from parsa.utils import text as txt
 
-class TextTestCase(unittest.TestCase):
-    def test_get_text(self):
-        """Test that """
-        return True
+class TextTest(unittest.TestCase):
+    def test_get_text_empty_file(self):
+        infile = tempfile.NamedTemporaryFile(suffix='.txt')
+        text = txt.get_text(infile.name)
+        # Empty strings evaluate to false
+        self.assertFalse(text)
+
+    def test_get_text_extension_not_supported(self):
+        infile = tempfile.NamedTemporaryFile(suffix='.abc')
+        
+        # Redirect stdout to a StringIO object
+        out_stringIO = io.StringIO()
+        sys.stdout = out_stringIO
+
+        txt.get_text(infile.name)
+
+        # Reset redirect
+        sys.stdout = sys.__stdout__
+        out_message = out_stringIO.getvalue()
+
+        self.assertIn('Extension not supported', out_message)
+    
+    def test_get_text_no_extension(self):
+        expected_text = 'test'
+        # This file has no file extension
+        infile = tempfile.NamedTemporaryFile(suffix=None)
+        # Write 'test' to the temporary file
+        with open(infile.name, 'wb') as f_in:
+            encoded_text = expected_text.encode('utf-8')
+            f_in.write(encoded_text)
+
+        # Python 2.x
+        if sys.version_info[0] < 3:
+            builtin_input = '__builtins__.raw_input'
+        # Python 3.x
+        else:
+            builtin_input = 'builtins.input'
+
+        # Call get_text, mocking the input for _infile_extension as txt when the function prompts for it
+        with unittest.mock.patch(builtin_input, return_value='txt'):
+            extracted_text = txt.get_text(infile.name)
+        self.assertEqual(extracted_text, expected_text)
     
     def test__process_text_utf8(self):
         """Test that utf-8 encoded text is successfully decoded."""
