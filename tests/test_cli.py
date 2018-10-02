221de5b805b271a6ba3faaf84584a3eca31ec30c
post-rick <riccardodimaio11@gmail.com>
2018-10-02 02:49:16 +0200

    Changed code from modifying sys.argv during runtime to mocking it with unittest.mock.patch

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -14,6 +14,11 @@ import unittest
 import os
 import sys
 
+if sys.version_info[0] < 3:
+    import mock
+else:
+    from unittest import mock
+
 sys.path.append(os.path.abspath('..'))
 from parsa.utils import cli
 
@@ -21,17 +26,20 @@ class CLITest(unittest.TestCase):
 
     def test_parse_arguments_empty_args(self): 
         """When sys.argvs is empty, the function should exit with SystemExit: 2"""                  
-        sys.argv = ['']
-        # https://stackoverflow.com/a/13491726
-        with self.assertRaises(SystemExit) as sys_e:
-            cli.parse_arguments()
-        self.assertEqual(sys_e.exception.code, 2)
 
-    def test_parse_arguments_args_output_not_passed(self):
-        cli_input = 'foo'
-        sys.argv[1] = cli_input
-        args = vars(cli.parse_arguments())
-        self.assertEqual(args['input'], cli_input)
+        testargs = ['']
+        with mock.patch.object(sys, 'argv', testargs):
+            # https://stackoverflow.com/a/13491726
+            with self.assertRaises(SystemExit) as sys_e:
+                cli.parse_arguments()
+            self.assertEqual(sys_e.exception.code, 2)
+
+    def test_parse_arguments_no_output_arg_passed(self):
+        cli_input_arg = 'foo'
+        testargs = ['', cli_input_arg]
+        with mock.patch.object(sys, 'argv', testargs):
+            args = vars(cli.parse_arguments())
+            self.assertEqual(args['input'], cli_input_arg)
 
     #def test_parse_arguments_args_output_passed(self):
     #    cli_input = 'foo'
@@ -41,3 +49,9 @@ class CLITest(unittest.TestCase):
     #    args = vars(cli.parse_arguments())
     #    self.assertEqual(args['input'], cli_input)
     #    self.assertEqual(args['output'], cli_output)
+
+    def test_parse_args(self):
+        testargs = ['', 'foo']
+        with mock.patch.object(sys, 'argv', testargs):
+            
+            return True
