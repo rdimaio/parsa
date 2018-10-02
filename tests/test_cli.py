7b7a7a9ca86940b534b492c216d94f2755d30521
post-rick <riccardodimaio11@gmail.com>
2018-10-02 03:59:58 +0200

    Complete test suit for cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,15 +5,15 @@ Tests:
         args_output_not_passed
         with_outdir
 
-
     _set_arguments:
-
+        tested implicitly in the parse_arguments test
 """
 
 import unittest
 import os
 import sys
-
+import inspect
+import argparse
 if sys.version_info[0] < 3:
     import mock
 else:
@@ -22,10 +22,18 @@ else:
 sys.path.append(os.path.abspath('..'))
 from parsa.utils import cli
 
+def get_default_args(func):
+    signature = inspect.signature(func)
+    return {
+        k: v.default
+        for k, v in signature.parameters.items()
+        if v.default is not inspect.Parameter.empty
+    }
+
 class CLITest(unittest.TestCase):
 
     def test_parse_arguments_empty_args(self): 
-        """When sys.argvs is empty, the function should exit with SystemExit: 2"""                  
+        """When sys.argvs is empty, the function should exit with SystemExit: 2."""                  
 
         testargs = ['']
         with mock.patch.object(sys, 'argv', testargs):
@@ -35,23 +43,21 @@ class CLITest(unittest.TestCase):
             self.assertEqual(sys_e.exception.code, 2)
 
     def test_parse_arguments_no_output_arg_passed(self):
+        """Only the input argument is passed."""
         cli_input_arg = 'foo'
         testargs = ['', cli_input_arg]
         with mock.patch.object(sys, 'argv', testargs):
             args = vars(cli.parse_arguments())
             self.assertEqual(args['input'], cli_input_arg)
 
-    #def test_parse_arguments_args_output_passed(self):
-    #    cli_input = 'foo'
-    #    cli_output = 'bar'
-    #    sys.argv[1] = cli_input
-    #    sys.argv.append(cli_output)
-    #    args = vars(cli.parse_arguments())
-    #    self.assertEqual(args['input'], cli_input)
-    #    self.assertEqual(args['output'], cli_output)
-
-    def test_parse_args(self):
-        testargs = ['', 'foo']
+    def test_parse_arguments_output_arg_passed(self):
+        """Both the input and output arguments are passed."""
+        cli_input_arg = 'foo'
+        cli_output_arg = 'bar'
+        testargs = ['', 
+                    '-o', cli_output_arg,
+                    cli_input_arg]
         with mock.patch.object(sys, 'argv', testargs):
-            
-            return True
+            args = vars(cli.parse_arguments())
+            self.assertEqual(args['input'], cli_input_arg)
+            self.assertEqual(args['output'], cli_output_arg)
