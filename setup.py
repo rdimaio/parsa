0546282c6f2dc0e10f1356a09cd05d8c924e34e9
post-rick <riccardodimaio11@gmail.com>
2018-10-09 03:14:44 +0200

    Update demo GIF for README

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='parsa',
       version='1.1.3',
       description='A multiformat text parser',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='http://github.com/rdimaio/parsa',
       author='Riccardo Di Maio',
       author_email='riccardodimaio11@gmail.com',
@@ -13,4 +18,20 @@ setup(name='parsa',
                   'parsa = parsa.parsa:main'
             ]
       },
-      zip_safe=False)
+      zip_safe=False,
+      classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "Intended Audience :: Information Technology",
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: System Administrators",
+            "Intended Audience :: Other Audience",
+            "Programming Language :: Python :: 2.7", 
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: POSIX",
+            "Topic :: Text Processing",
+            "Topic :: Utilities",
+      ],
+)
